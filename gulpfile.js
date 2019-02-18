b4e02a6097a7d2e0bcde4936ba5679bdf9a59ed7
Anastasiia Kulachok <kulachokn@gmail.com>
2019-02-18 17:07:43 +0200

    blocks

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
+var gulpResolveUrl = require('gulp-resolve-url');
 
 sass.compiler = require('node-sass');
 
@@ -17,6 +18,7 @@ function style() {
     return gulp.src('./sass/style.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(gulpResolveUrl())
         .pipe(concat('style.css'))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./css'));
